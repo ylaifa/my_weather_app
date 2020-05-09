f094524edaab7811d2f3e2b01ed33f95d22c03ef
Yousri <laifayousri@gmail.com>
2020-05-10 00:02:26 +0200

    add react-loader-spiner

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
-class App extends React.Component {
-  render() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("update isLoading");
+  }, [isLoading]);
+
+  const changeLoadingStaut = () => {
+    setIsLoading(false);
+    console.log(isLoading);
+  };
+
+  if (isLoading) {
+    return (
+      <>
+        <Loader type="Puff" color="#00BFFF" height={100} width={100} />
+        <p>Veuillez patienter, nous sommes entrain de vous localiser...</p>
+      </>
+    );
+  } else {
     return (
       <>
         <div>Hello World !!</div>
       </>
     );
   }
-}
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
