70f6998383d12246bf2196e22d55d23a01ced197
Yousri <laifayousri@gmail.com>
2020-05-10 13:59:27 +0200

    edit src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 
+const apikey = "124d36c5bbebe70fb4caa75b0a1e9b8a";
+
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    console.log("update isLoading");
+    changeStatutLoading();
   }, [isLoading]);
 
-  const changeLoadingStaut = () => {
-    setIsLoading(false);
-    console.log(isLoading);
+  const changeStatutLoading = () => {
+    if (navigator.geolocation.getCurrentPosition) {
+      setIsLoading(false);
+    }
   };
 
   if (isLoading) {
