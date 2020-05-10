2777d878839b74a4f45e765309306d9cedd295f2
Yousri <laifayousri@gmail.com>
2020-05-10 20:09:41 +0200

    edit src/index.jsx fetch api

--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Row, Col } from "antd";
 import "antd/dist/antd.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 
-// const apikey = "124d36c5bbebe70fb4caa75b0a1e9b8a";
+class App extends Component {
+  constructor() {
+    super();
 
-const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
+    this.state = {
+      isLoading: true
+    };
+  }
 
-  useEffect(() => {
-    changeStatutLoading();
-  }, [isLoading]);
+  componentDidMount = () => {
+    const apikey = "124d36c5bbebe70fb4caa75b0a1e9b8a";
+    console.log("hello");
 
-  const changeStatutLoading = () => {
-    if (navigator.geolocation.getCurrentPosition) {
-      setIsLoading(false);
-    }
+    navigator.geolocation.getCurrentPosition(position => {
+      var coord = position.coords;
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coord.latitude}&lon=${coord.longitude}&appid=${apikey}`
+      )
+        .then(response => response.json())
+        .then(response => {
+          console.log(response);
+        })
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
+    });
   };
 
-  if (isLoading) {
-    return (
-      <>
-        <Row type="flex" align="middle">
-          <Col span={24}>
-            <Loader type="Circles" color="#00BFFF" height={80} width={80} />
-          </Col>
-          <Col span={10}>
-            Veuillez patienter, nous sommes entrain de vous localiser...
-          </Col>
-        </Row>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div>Hello World !!</div>
-      </>
-    );
+  render() {
+    let renderedView;
+    const { isLoading } = this.state;
+    if (isLoading) {
+      renderedView = (
+        <>
+          <Row type="flex" align="middle">
+            <Col span={24}>
+              <Loader type="Circles" color="#00BFFF" height={80} width={80} />
+            </Col>
+            <Col span={10}>
+              Veuillez patienter, nous sommes entrain de vous localiser...
+            </Col>
+          </Row>
+        </>
+      );
+    } else {
+      renderedView = (
+        <div>
+          <h3>Hello World !!</h3>
+        </div>
+      );
+    }
+    return <>{renderedView}</>;
   }
-};
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
